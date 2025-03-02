9705230703807dc9e056e618ab7a16b7e1506ef5
DhruvPokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2025-03-02 09:22:36 +0530

    Create Admin class that extends Person class

---
+++ src/main/java/com/iit/banking/entity/Admin.java
@@ -0,0 +1,46 @@
+package com.iit.banking.entity;
+
+import java.util.Objects;
+
+import jakarta.persistence.*;
+
+@Entity
+@SequenceGenerator(name = "admin_sequence", sequenceName = "admin_sequence", allocationSize = 1)
+public class Admin extends Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "admin_sequence")
+    private Long id;
+
+    public Admin() {
+    }
+
+    public Admin(String name, String email, String password) {
+        super(name, email, password);
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Admin admin = (Admin) o;
+        return id.equals(admin.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, password);
+    }
+
+}
