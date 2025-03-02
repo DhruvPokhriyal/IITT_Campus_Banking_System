07b5b0b20bfa91fbe8d27a5b39cd3afe77cc5c0a
DhruvPokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2025-03-02 12:06:51 +0530

    Create UserDTO to show only necessary details

--- src/main/java/com/iit/banking/dto/UserDTO.java
+++ src/main/java/com/iit/banking/dto/UserDTO.java
@@ -1,5 +1,48 @@
 package com.iit.banking.dto;
 
+import com.iit.banking.model.entity.Account;
+import com.iit.banking.model.entity.User;
+
 public class UserDTO {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String accountNumber;
+    private Double balance;
+
+    public UserDTO(User user) {
+        this.id = user.getId();
+        this.name = user.getName();
+        this.email = user.getEmail();
+        Account account = user.getAccount();
+        if (account != null) {
+            this.accountNumber = account.getAccountNumber();
+            this.balance = account.getBalance();
+        } else {
+            this.accountNumber = null;
+            this.balance = 0.0;
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
 
 }
