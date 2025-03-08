7755e1b010bad45ec5b7a019eca80669827f0204
DhruvPokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2025-03-08 14:27:08 +0530

    Add deposit, withdraw and transfer functionality in TransactionService

--- src/main/java/com/iit/banking/service/TransactionService.java
+++ src/main/java/com/iit/banking/service/TransactionService.java
@@ -5,8 +5,12 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.iit.banking.dto.TransactionDTO;
+import com.iit.banking.model.entity.Account;
+import com.iit.banking.model.entity.Transaction;
 import com.iit.banking.repository.AccountRepository;
 import com.iit.banking.repository.TransactionRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class TransactionService {
@@ -22,4 +26,44 @@ public class TransactionService {
         return transactionRepository.findBySenderOrReceiverId(accountId, accountId).stream().map(TransactionDTO::new)
                 .toList();
     }
+
+    @Transactional
+    public TransactionDTO deposit(Long accountId, Double amount) {
+        Account account = accountRepository.findById(accountId).orElseThrow();
+        account.setBalance(account.getBalance() + amount);
+        accountRepository.save(account);
+        Transaction transaction = new Transaction("Deposit", amount, null, null, account);
+        Transaction savedTransaction = transactionRepository.save(transaction);
+        return new TransactionDTO(savedTransaction);
+    }
+
+    @Transactional
+    public TransactionDTO withdraw(Long accountId, Double amount) {
+        Account account = accountRepository.findById(accountId).orElseThrow();
+        if (account.getBalance() < amount) {
+            throw new Error("Insufficient balance");
+        }
+        account.setBalance(account.getBalance() - amount);
+        accountRepository.save(account);
+        Transaction transaction = new Transaction("Withdraw", amount, null, null, account);
+        Transaction savedTransaction = transactionRepository.save(transaction);
+        return new TransactionDTO(savedTransaction);
+    }
+
+    @Transactional
+    public TransactionDTO transfer(Long senderId, Long receiverId, Double amount) {
+        Account sender = accountRepository.findById(senderId).orElseThrow();
+        Account receiver = accountRepository.findById(receiverId).orElseThrow();
+        if (sender.getBalance() < amount) {
+            throw new Error("Insufficient balance");
+        }
+        sender.setBalance(sender.getBalance() - amount);
+        receiver.setBalance(receiver.getBalance() + amount);
+        accountRepository.save(sender);
+        accountRepository.save(receiver);
+        Transaction transaction = new Transaction("Transfer", amount, null, sender, receiver);
+        Transaction savedTransaction = transactionRepository.save(transaction);
+        return new TransactionDTO(savedTransaction);
+    }
+
 }
