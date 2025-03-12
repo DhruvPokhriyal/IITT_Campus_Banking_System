51ac73c17ed674e8273b91725ad3d50b9bbd3b12
DhruvPokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2025-03-12 20:31:34 +0530

    Create LoginRequestDTO file to contain loginRequest Data

--- src/main/java/com/iit/banking/service/AuthService.java
+++ src/main/java/com/iit/banking/service/AuthService.java
@@ -1,5 +1,26 @@
 package com.iit.banking.service;
 
-public class AuthService {
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+import com.iit.banking.dto.LoginRequestDTO;
+import com.iit.banking.dto.LoginResponseDTO;
+import com.iit.banking.repository.UserRepository;
+import com.iit.banking.util.JwtUtil;
+
+@Service
+public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
+    }
+
+    public LoginResponseDTO login(LoginRequestDTO loginRequest) {
+        User user = userRepository.findByEmail(loginRequest.getEmail());
+    }
 }
