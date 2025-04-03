8030d620672dadf406484e87db9aff32d3786540
DhruvPokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2025-04-03 13:49:05 +0530

    Update OpenAPI configuration to change contact email for IIT Banking Team

--- src/main/java/com/iit/banking/config/OpenApiConfig.java
+++ src/main/java/com/iit/banking/config/OpenApiConfig.java
@@ -14,24 +14,24 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenApiConfig {
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("IIT Banking System API")
-                        .version("1.0")
-                        .description("API documentation for IIT Banking System")
-                        .contact(new Contact()
-                                .name("IIT Banking Team")
-                                .email("support@iitbanking.com"))
-                        .license(new License()
-                                .name("Apache 2.0")
-                                .url("http://www.apache.org/licenses/LICENSE-2.0.html")))
-                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
-                .components(new Components()
-                        .addSecuritySchemes("Bearer Authentication", new SecurityScheme()
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")));
-    }
+        @Bean
+        public OpenAPI customOpenAPI() {
+                return new OpenAPI()
+                                .info(new Info()
+                                                .title("IIT Banking System API")
+                                                .version("1.0")
+                                                .description("API documentation for IIT Banking System")
+                                                .contact(new Contact()
+                                                                .name("IIT Banking Team")
+                                                                .email("dhruvpokhriyaltech@gmail.com"))
+                                                .license(new License()
+                                                                .name("Apache 2.0")
+                                                                .url("http://www.apache.org/licenses/LICENSE-2.0.html")))
+                                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                                .components(new Components()
+                                                .addSecuritySchemes("Bearer Authentication", new SecurityScheme()
+                                                                .type(SecurityScheme.Type.HTTP)
+                                                                .scheme("bearer")
+                                                                .bearerFormat("JWT")));
+        }
 }
